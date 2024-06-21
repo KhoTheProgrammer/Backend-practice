ee8d14ae6710576f1bd79348399a941e0af02114
KhoTheProgrammer <kondwanipadyera@gmail.com>
2024-06-21 14:53:52 +0200

    Read files using async and await

---
+++ Async-patterns/3-await-pattern.js
@@ -0,0 +1,24 @@
+const { readFile } = require("fs");
+
+const getText = (path) => {
+  return new Promise((resolve, reject) => {
+    readFile(path, "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
+
+const start = async () => {
+  try {
+    const first = await getText("./content/first.txt");
+    const second = await getText("./content/second.txt");
+    console.log(first, second);
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
