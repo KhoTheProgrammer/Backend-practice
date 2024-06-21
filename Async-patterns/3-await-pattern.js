de3fbe10d539905638490771bdf9a2f4c1948cdd
KhoTheProgrammer <kondwanipadyera@gmail.com>
2024-06-21 14:56:20 +0200

    Used promisify function of util module to turn read and write functions into promises and removed getText function

--- Async-patterns/3-await-pattern.js
+++ Async-patterns/3-await-pattern.js
@@ -1,21 +1,12 @@
-const { readFile } = require("fs");
-
-const getText = (path) => {
-  return new Promise((resolve, reject) => {
-    readFile(path, "utf8", (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    });
-  });
-};
+const { readFile, writeFile} = require("fs");
+const util = require('util');
+const readFilePromise = util.promisify(readFile);
+const writeFilePromise = util.promisify(writeFile);
 
 const start = async () => {
   try {
-    const first = await getText("./content/first.txt");
-    const second = await getText("./content/second.txt");
+    const first = await readFilePromise("./content/first.txt", 'utf8');
+    const second = await readFilePromise("./content/second.txt", 'utf8');
     console.log(first, second);
   } catch (error) {
     console.log(error);
