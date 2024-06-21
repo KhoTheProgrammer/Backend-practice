f494fa489956542dbf18bba4940c9cf0dda3957c
KhoTheProgrammer <kondwanipadyera@gmail.com>
2024-06-21 15:02:03 +0200

    read files using promises

---
+++ Async-patterns/2-promise-pattern.js
@@ -0,0 +1,21 @@
+const { readFile } = require("fs");
+
+const getText = (path) => {
+  return new Promise((resolve, reject) => {
+    readFile(path, "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
+
+getText("./content/first.txt")
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
