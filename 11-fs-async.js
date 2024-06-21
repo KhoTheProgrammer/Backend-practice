78e39993edf787766d6d086af5aa8589c1d65ab3
KhoTheProgrammer <kondwanipadyera@gmail.com>
2024-06-21 10:08:33 +0200

    Read and write files asynchronously using functions found in fs modules

---
+++ 11-fs-async.js
@@ -0,0 +1,29 @@
+const { readFile, writeFile } = require("fs");
+
+console.log('start');
+readFile("./context/first.txt", "utf8", (err, result) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const first = result;
+  readFile("./context/second.txt", "utf8", (err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const second = result;
+    writeFile(
+      "./context/result.txt",
+      `Here is the result: ${first}, ${second}`,
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log('done with this task');
+      }
+    );
+  });
+});
+console.log('starting next task');
